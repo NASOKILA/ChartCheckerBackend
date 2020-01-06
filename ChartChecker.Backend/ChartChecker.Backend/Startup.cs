85ead1fa558c90fd45ca0bde9c0dcc205d4bbaa9
Atanas <atanasskambitovv@gmail.com>
2020-01-07 01:10:40 +0200

    files controller created

--- ChartChecker.Backend/ChartChecker.Backend/Startup.cs
+++ ChartChecker.Backend/ChartChecker.Backend/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -15,9 +16,20 @@ namespace ChartChecker.Backend
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment hostingEnvironment)
         {
-            Configuration = configuration;
+            var environment = Environment.GetEnvironmentVariable("Env") ?? "local";
+
+            Console.WriteLine($"Environment: {environment}");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+                .AddUserSecrets<Startup>()
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -26,6 +38,18 @@ namespace ChartChecker.Backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var connectionUrl = Configuration.GetValue<string>("Environment:Url");
+
+            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
+            {
+
+                builder
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithOrigins(connectionUrl)
+                .AllowCredentials();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
